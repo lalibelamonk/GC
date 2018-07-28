7d1b9ecee1220e4a7a7957d94c9d83b06fb23eca
Neal Johnson <nrjohnso@mtu.edu>
2018-07-28 13:20:04 -0400

    Initial commit

---
+++ Assets/Scripts/DataController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DataController : MonoBehaviour {
+
+	public SessionData[] sessions;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (gameObject);
+		SceneManager.LoadScene("Menu");
+	}
+
+	public SessionData GetCurrentRoundData() {
+		return sessions[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
---
+++ Assets/Scripts/SessionData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SessionData {
+
+	public string id;
+	public int totalGuilt;
+	public QuestionData[] questions;
+}
+
